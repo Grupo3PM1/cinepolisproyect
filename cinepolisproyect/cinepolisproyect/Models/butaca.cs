95671eb955620f7670294148e64df60a30476caf
Eduardo251 <91290449+Eduardo251@users.noreply.github.com>
2021-11-27 01:25:10 -0600

    vais ah flipah con loh votoneh

---
+++ cinepolisproyect/cinepolisproyect/Models/butaca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cinepolisproyect.Models
+{
+    public class butaca
+    {
+        public string a1 { get; set; }
+        public string a2 { get; set; }
+        public string a3 { get; set; }
+        public string a4 { get; set; }
+        public string a5 { get; set; }
+        public string a6 { get; set; }
+        public string a7 { get; set; }
+    }
+}
--- cinepolisproyect/cinepolisproyect/Views/ButacaPage.xaml.cs
+++ cinepolisproyect/cinepolisproyect/Views/ButacaPage.xaml.cs
@@ -13,27 +13,144 @@ namespace cinepolisproyect.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ButacaPage : ContentPage
     {
+        Button btn;
+        public Models.butaca butacadata;
         public ButacaPage(/*Ticket ticket*/)
         {
             
             InitializeComponent();
+            butacadata = new Models.butaca 
+            {
+                a1 = "1",
+                a2 = "1",
+                a3 = "1",
+                a4 = "0",
+                a5 = "1",
+                a6 = "0",
+                a7 = "0",
+            };
 
+            if (butacadata.a1 == "1")
+                this.seat1.BackgroundColor = Color.FromHex("#FFA500");
+            else
+                this.seat1.BackgroundColor = Color.Transparent;
+
+            if (butacadata.a2 == "1")
+                this.seat2.BackgroundColor = Color.FromHex("#FFA500");
+            else
+                this.seat2.BackgroundColor = Color.Transparent;
+
+            if (butacadata.a3 == "1")
+                this.seat3.BackgroundColor = Color.FromHex("#FFA500");
+            else
+                this.seat3.BackgroundColor = Color.Transparent;
+
+            if (butacadata.a4 == "1")
+                this.seat4.BackgroundColor = Color.FromHex("#FFA500");
+            else
+                this.seat4.BackgroundColor = Color.Transparent;
+
+            if (butacadata.a5 == "1")
+                this.seat5.BackgroundColor = Color.FromHex("#FFA500");
+            else
+                this.seat5.BackgroundColor = Color.Transparent;
+
+            if (butacadata.a6 == "1")
+                this.seat6.BackgroundColor = Color.FromHex("#FFA500");
+            else
+                this.seat6.BackgroundColor = Color.Transparent;
+
+            if (butacadata.a7 == "1")
+                this.seat7.BackgroundColor = Color.FromHex("#FFA500");
+            else
+                this.seat7.BackgroundColor = Color.Transparent;
             /*SelectedTicket = ticket;
             Init();
             this.BindingContext = this;*/
 
         }
 
+
+
         private async void btncomprar_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new Views.ProductosPage());
-        }
-
-
+            await Navigation.PushAsync(new Views.TiendaPage());
+        } 
 
         private void seat1_Clicked(object sender, EventArgs e)
         {
+            if (butacadata.a1 == "1")
+                DisplayAlert("Advertencia", "El asiento ya esta ocupado", "Ok");
+            else
+            {
+                btn = (Button)sender;
+                if (btn.BackgroundColor.Equals(Color.Transparent))
+                { 
+                    btn.BackgroundColor = Color.FromHex("#FFFF00");
+                    butacadata.a1 = "1";
+                }
+                else
+                    btn.BackgroundColor = Color.Transparent;
+            }
         }
 
+        private void seat2_Clicked(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (btn.BackgroundColor.Equals(Color.Transparent))
+                btn.BackgroundColor = Color.FromHex("#FFFF00");
+            else
+                btn.BackgroundColor = Color.Transparent;
+
+        }
+
+        private void seat3_Clicked(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (btn.BackgroundColor.Equals(Color.Transparent))
+                btn.BackgroundColor = Color.FromHex("#FFFF00");
+            else
+                btn.BackgroundColor = Color.Transparent;
+        }
+
+        private void seat4_Clicked(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (btn.BackgroundColor.Equals(Color.Transparent))
+                btn.BackgroundColor = Color.FromHex("#FFFF00");
+            else
+                btn.BackgroundColor = Color.Transparent;
+        }
+
+        private void seat5_Clicked(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (btn.BackgroundColor.Equals(Color.Transparent))
+                btn.BackgroundColor = Color.FromHex("#FFFF00");
+            else
+                btn.BackgroundColor = Color.Transparent;
+        }
+
+        private void seat6_Clicked(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (btn.BackgroundColor.Equals(Color.Transparent))
+                btn.BackgroundColor = Color.FromHex("#FFFF00");
+            else
+                btn.BackgroundColor = Color.Transparent;
+
+        }
+
+        private void seat7_Clicked(object sender, EventArgs e)
+        {
+            btn = (Button)sender;
+            if (btn.BackgroundColor.Equals(Color.Transparent))
+                btn.BackgroundColor = Color.FromHex("#FFFF00");
+            else
+                btn.BackgroundColor = Color.Transparent;
+
+        }
+
+      
     }
 }
