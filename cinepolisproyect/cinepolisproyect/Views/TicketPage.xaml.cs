11aea5c50f7d50d1aa7efe2487243fb8c48f8fc7
Stephany2000 <calixstephany2000@gmail.com>
2021-12-03 14:01:09 -0600

    QR

--- cinepolisproyect/cinepolisproyect/Views/TicketPage.xaml.cs
+++ cinepolisproyect/cinepolisproyect/Views/TicketPage.xaml.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
 
 namespace cinepolisproyect.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TicketPage : ContentPage
     {
+        ZXingBarcodeImageView qr;
         public TicketPage()
         {
             InitializeComponent();
+        }
+
+        private async void btngenerarcodigo_Clicked(object sender, EventArgs e)
+        {
+            qr = new ZXingBarcodeImageView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            qr.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            qr.BarcodeOptions.Width = 300;
+            qr.BarcodeOptions.Height = 300;
+            qr.BarcodeValue = "https://www.youtube.com.mx";
+            stkQR.Children.Add(qr);
+
         }
     }
 }
